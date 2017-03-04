91fa17310f3912ac5d67ea0547301700bb84f4be
Isa Deu <ipd220@nyu.edu>
2017-03-04 18:27:11 -0500

    the camera is beautiful now god bless

---
+++ Assets/scripts/newCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newCamera : MonoBehaviour {
+	private Vector2 velocity;
+	public float smoothTimeY;
+	public float smoothTimeX;
+	public GameObject mainChara;
+
+	void Start () {
+		//mainChara = GameObject.FindGameObjectsWithTag ("Player");
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate (){
+		//not rough
+		float posX = Mathf.SmoothDamp(transform.position.x, mainChara.transform.position.x, ref velocity.x, smoothTimeX);
+		float posY = Mathf.SmoothDamp(transform.position.y, mainChara.transform.position.y, ref velocity.y, smoothTimeY);
+
+		transform.position = new Vector3 (posX, posY, transform.position.z);
+
+	}
+}
+	
