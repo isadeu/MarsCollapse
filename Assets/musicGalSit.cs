044fe86bbb46cd2bbfcaa99211568818e3d0c02e
Isa Deu <ipd220@nyu.edu>
2017-03-30 20:17:18 -0400

    she sits,, in place

--- Assets/musicGalSit.cs
+++ Assets/musicGalSit.cs
@@ -2,55 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class followerScript : MonoBehaviour {
-
-	//moving behind
+public class musicGalSit : MonoBehaviour {
+	
 	Rigidbody2D rb;
 	public GameObject follower;
 	public GameObject mainChara;
 	public List<Vector2> pointList;
 	//beforefollowing
 	bool following = false;
-	public float startLx;
-	public float startLy;
-	public float endLx;
-	public float endLy;
-	public float speed;
-	//delay
+
 	bool delay;
 	public float WaitTime = .3f;
 
+	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();	
+		
 	}
-
-
-
+	
 	// Update is called once per frame
 	void Update () {
-		
-		//keep from rolling
+
 		transform.rotation = Quaternion.identity;
-
-		//when not following
-		if (following == false){
-			Vector2 fromM = new Vector2 (startLx, startLy); //starts here
-			Vector2 toM = new Vector2 (endLx, endLy); //goes to here and loops
-			transform.position = Vector2.Lerp (fromM, toM, Mathf.PingPong (Time.time * speed, 2));
-		}
-
-
-//		//delay supposedly
-//		 StartCoroutine(smallDelay());
-//	}
-//
-//	IEnumerator smallDelay() {
-//		print(Time.time);
-//		yield return new WaitForSeconds(3f);
-//		print(Time.time);
-		//ja està
+		
 	}
-
 
 	void FixedUpdate () {
 
@@ -58,7 +33,7 @@ public class followerScript : MonoBehaviour {
 			gameObject.layer = LayerMask.NameToLayer("following");
 			pointList.Add (mainChara.transform.position);
 			StartCoroutine ("FollowerDelay");
-		
+
 		}
 	}
 
@@ -71,12 +46,8 @@ public class followerScript : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D c){
 		if (c.gameObject.name == "colliderobjects") {//if it triggers contact with player, player will become the parent of this object
-			Debug.Log ("entered trigger god bless");
+			Debug.Log ("entered trigger god bless 2 ");
 			following = true;
 		}
 	}
 }
-
-//jump back to me b; the problem 
-//		if (mainChara.position	 + 3) {
-//			transform.position = Vector2.Lerp (mainChara.position);
