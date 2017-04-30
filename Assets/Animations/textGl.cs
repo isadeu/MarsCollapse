0cd479f60a1ab1e985eeb80d1e3c7b892db3f78d
Isa Deu <ipd220@nyu.edu>
2017-04-29 23:55:03 -0400

    Code godess

--- Assets/Animations/textGl.cs
+++ Assets/Animations/textGl.cs
@@ -7,6 +7,7 @@ public class textGl : MonoBehaviour {
 
 	public Sprite  [] texts;
 	private SpriteRenderer currentSprite;
+	private SpriteRenderer currentSprite2;
 	private int counter;
 	public bool goTexts;
 	public GameObject gal;
@@ -17,8 +18,16 @@ public class textGl : MonoBehaviour {
 	public Sprite three; 
 	public Sprite four;
 
+
+	public Sprite  [] texts2;
+	public bool onBoy;
+	public Sprite one2;
+	public Sprite two2;
+	public Sprite three2;
+
 	void Start(){
 		//goTexts = false;
+		//gal
 		counter = 0;
 		texts = new Sprite[4];
 		texts [0] = one;//transparent
@@ -26,30 +35,68 @@ public class textGl : MonoBehaviour {
 		texts [2] = three;
 		texts [3] = four; 
 
-	
+		//boy
+		texts2 = new Sprite[3];
+		texts2 [0] = one2;//transparent
+		texts2 [1] = two2;
+		texts2 [2] = three2; 
+
+
 
 		Debug.Log (texts.Length);
 		currentSprite = GetComponent <SpriteRenderer> ();
 		//texts = new List<Sprite>();
 		//counter = 0;
+
+
+		currentSprite2 = GetComponent <SpriteRenderer> ();
+
 	}
 
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.G)&& goTexts == true){//&& onGal == true) {
+//
+//		if (onGal == false || onBoy == false) {
+//			currentSprite.sprite = one;
+//			currentSprite2.sprite = one;
+//		}
+
+
+
+		if (Input.GetKeyDown (KeyCode.G) && goTexts == true) {//&& onGal == true) {
 			counter++;
 			Debug.Log ("code G");
 		} 
 
-		if (counter < texts.Length) { //&& goTexts == false) {
+
+
+
+		if (onGal == true && counter < texts.Length) { //&& goTexts == false) {
 			currentSprite.sprite = texts [counter % texts.Length];
 			//goTexts = false;
 		}
 
-		if (counter > 3) {//texts.Length) {
+
+		if (onGal == true && counter > 3) {//texts.Length) {
 			//currentSprite.sprite = four;
-			Debug.Log ("??");
+			currentSprite.sprite = texts [0];
 			counter = 0;
 			goTexts = false;
+			onGal = false;
+		}
+
+
+
+		if (onBoy == true && counter < texts2.Length) { //&& goTexts == false) {
+			currentSprite2.sprite = texts2 [counter % texts2.Length];
+			//goTexts = false;
+		}
+
+		if (onBoy == true && counter > 2) {
+			Debug.Log ("boy counter stopped");
+			currentSprite.sprite = texts [0];
+			counter = 0;
+			goTexts = false;
+			onBoy = false;
 		}
 	}
 
@@ -60,7 +107,17 @@ public class textGl : MonoBehaviour {
 			Destroy (c.gameObject);
 			onGal = true;
 			goTexts = true;
+			onBoy = false;
+			//onBoy = false;
 		}
+		
+		if (c.gameObject.name == "boyCollider") {
+			Debug.Log ("boy");
+			Destroy (c.gameObject);
+			onBoy = true;
+			onGal = false;
+			goTexts = true;
+			}
 			
 
 
